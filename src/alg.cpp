48ec9121c2886dbcf553c8a67193890463bcd3b0
Артемий Алонцев <79205183+hardBASSmachine@users.noreply.github.com>
2021-03-09 10:41:56 +0300

    Update alg.cpp
    Алонцев Артемий

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,12 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+ Integer gcd (Integer a, Integer b) {
+	 for (Integer c; b; ) {
+		 c = a % b;
+		 a = b;
+		 b = c;
+	 }
+	 return abs(a);
+ }
 }
