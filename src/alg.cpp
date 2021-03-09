b1e61deff64f163451ae812c2a8580978d23fe07
Артемий Алонцев <79205183+hardBASSmachine@users.noreply.github.com>
2021-03-09 10:59:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,11 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
- Integer gcd (Integer a, Integer b) {
-	 for (Integer c; b; ) {
-		 c = a % b;
-		 a = b;
-		 b = c;
-	 }
-	 return abs(a);
+int c;
+while (!(a % b) == 0) {
+      c = a;
+      a = b;
+      b = c % b;
  }
+return b;
 }
